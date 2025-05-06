5b95fd7356a558396e843abeda9a8c0192625049
riverDeer12 <mtrbojevic12@gmail.com>
2025-05-06 15:35:01 +0200

    refactored part of parsing roles from jwt token

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,60 +1,60 @@
 import {
-    ActivatedRouteSnapshot,
-    CanActivate,
-    Router,
-    RouterStateSnapshot
-} from '@angular/router';
-import {Injectable} from "@angular/core";
-import {AuthenticationService} from "../features/authentication/services/authentication.service";
-import {Roles} from "../constants/roles";
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { Injectable } from "@angular/core";
+import { AuthenticationService } from "../features/authentication/services/authentication.service";
+import { Roles } from "../constants/roles";
+
 /**
  * Auth guard for admin routes.
  */
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class AdminGuard implements CanActivate {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) {}
 
-    constructor(private authenticationService: AuthenticationService,
-                private router: Router) {
+  /**
+   * Method that controls
+   * user's authorization state.
+   * @param route route that needs to be checked.
+   * @param _state
+   */
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot,
+  ): boolean | Promise<boolean> {
+    if (!this.authenticationService.isUserLogged()) {
+      this.router.navigateByUrl("/authentication/login").then();
+      return false;
+    } else {
+      return this.validateAccess();
+    }
+  }
+
+  /**
+   * Check if user
+   * has valid required
+   * policies.
+   *
+   * @returns true if user has valid required policies.
+   */
+  validateAccess(): boolean {
+    const adminRoles = [Roles.SuperAdmin, Roles.Administrator];
+    const userRoles = this.authenticationService.getLoggedUserRoles();
+
+    if (userRoles.includes(Roles.SuperAdmin)) {
+      return true;
     }
 
-    /**
-     * Method that controls
-     * user's authorization state.
-     * @param route route that needs to be checked.
-     * @param _state
-     */
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        _state: RouterStateSnapshot
-    ): boolean | Promise<boolean> {
-
-        if (!this.authenticationService.isUserLogged()) {
-            this.router.navigateByUrl('/authentication/login').then();
-            return false;
-        } else {
-            return this.validateAccess();
-        }
-    }
-
-    /**
-     * Check if user
-     * has valid required
-     * policies.
-     *
-     * @returns true if user has valid required policies.
-     */
-    validateAccess(): boolean {
-        const adminRoles = [Roles.SuperAdmin, Roles.Administrator];
-        const userRoles = this.authenticationService.getLoggedUserRoles();
-
-        console.log(userRoles);
-
-        if(userRoles.includes(Roles.SuperAdmin)) {
-            return true;
-        }
-
-        return userRoles.some(role => adminRoles.some(adminRole => role.includes(adminRole)));
-    }
+    return userRoles.some((role) =>
+      adminRoles.some((adminRole) => role.includes(adminRole)),
+    );
+  }
 }
