adf675590e4d3a1b7bd6aef2ac773170b40a05eb
mtrbojevic12 <mtrbojevic12@gmail.com>
2025-09-22 10:23:12 +0200

    clear shopping cart after successful payment updated payment site

--- src/app/features/landing/components/successful-payment/successful-payment.component.ts
+++ src/app/features/landing/components/successful-payment/successful-payment.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ShoppingCartService } from "../../services/shopping-cart.service";
 
 @Component({
-  selector: 'app-successful-payment',
+  selector: "app-successful-payment",
   imports: [],
-  templateUrl: './successful-payment.component.html',
-  styleUrl: './successful-payment.component.scss'
+  templateUrl: "./successful-payment.component.html",
+  styleUrl: "./successful-payment.component.scss",
 })
 export class SuccessfulPaymentComponent {
-
+  constructor(private shoppingCartService: ShoppingCartService) {
+    this.shoppingCartService.clearShoppingCart();
+  }
 }
