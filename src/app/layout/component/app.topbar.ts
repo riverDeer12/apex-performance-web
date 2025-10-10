1c670d35774084bc9927ba7f7657204fcf6b902d
mtrbojevic12 <mtrbojevic12@gmail.com>
2025-10-10 12:19:45 +0200

    updated logo

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -34,8 +34,8 @@ import { ButtonLabel } from "primeng/button";
         <img
           [ngSrc]="
             !layoutService.isDarkTheme()
-              ? 'assets/images/logo_light_theme.png'
-              : 'assets/images/logo_dark_theme.png'
+              ? 'assets/images/logo_light.png'
+              : 'assets/images/logo_dark.png'
           "
           width="200"
           height="50"
