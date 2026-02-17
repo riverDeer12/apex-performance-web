08b47f06e5c997d0745884d53618a324e501a09a
Milan Trbojevic <mtrbojevic12@gmail.com>
2026-02-17 13:38:42 +0100

    updated pipeline name

--- src/app/features/coaches/models/coach.ts
+++ src/app/features/coaches/models/coach.ts
@@ -13,9 +13,7 @@ export class Coach {
     user!: User;
     isDeleted!: boolean;
 
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
+    fullName!: string;
 
     get status(): string {
         return this.isDeleted ? 'Inactive' : 'Active';
