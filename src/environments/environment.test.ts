bf33e797a5d5f5330b0d60c61bd08d4bd3ac7509
Milan Trbojevic <mtrbojevic12@gmail.com>
2026-03-31 17:53:46 +0200

    refactoring

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: "https://test.apex-performance.fit/api"
+  apiUrl: "https://test.apex-performance.fit/api",
+  stripe: {
+    publishableKey:
+      "pk_test_51RHmsXARY6iIldCB99XK6sNUHMhIPqJTXUWEczue4Tm5eDUYfWnNLfZJgB4UmjDeHXaxtfLUkjiyEayve4sMyhUo008T9IBRfy",
+  },
 };
