10faededaedf107efbf18046c978fbc3414d715c
Milan Trbojevic <mtrbojevic12@gmail.com>
2026-04-16 13:34:04 +0200

    bugfixing for new version of stripe

--- src/app/features/landing/services/payment.service.ts
+++ src/app/features/landing/services/payment.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { switchMap } from "rxjs";
 import { DefaultPostRequest } from "../../../shared/models/default-post-request";
-import { loadStripe, Stripe } from "@stripe/stripe-js";
 import { environment } from "../../../../environments/environment";
+import {loadStripe, Stripe} from "@stripe/stripe-js";
 
 @Injectable({
   providedIn: "root",
@@ -19,26 +19,13 @@ export class PaymentService {
     this.stripe = await loadStripe(environment.stripe.publishableKey);
   }
 
-  createCheckoutSession(request: DefaultPostRequest) {
-    return this.http
-      .post<{ sessionId: string }>(environment.apiUrl + "/payments", request)
-      .pipe(
-        switchMap(async (response) => {
-          const stripe = await this.stripe;
-
-          if (!stripe) {
-            throw new Error("Stripe failed to initialize");
-          }
-
-          const { error } = await stripe.redirectToCheckout({
-            sessionId: response.sessionId,
-
-          });
-
-          if (error) {
-            throw new Error(error.message);
-          }
-        }),
-      );
-  }
+    createCheckoutSession(request: DefaultPostRequest) {
+        return this.http
+            .post<{ url: string }>(environment.apiUrl + "/payments", request)
+            .pipe(
+                switchMap(async (response) => {
+                    window.location.href = response.url;
+                })
+            );
+    }
 }
